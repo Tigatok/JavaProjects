cdfdc4aee312f8e6bef7a0fcfafb67bf9b3ee476
Tyler M <tyler.marshall2@myokanagan.bc.ca>
2014-02-21 11:00:55 -0800

    Adding MORE java projects

---
+++ Fraction/src/Fraction.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author Tyler Marshall 300210747
+ */
+public class Fraction {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+        public Fraction multiply(Fraction f){
+            //return type
+            return f;
+            }
+}
---
+++ GradeBook/src/GradeBook.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author tyler
+ */
+public class GradeBook 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
